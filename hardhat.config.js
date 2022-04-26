4f80e57f9ef5b878a653cf7206bd80168c5049c4
Josh <joshliamnice@gmail.com>
2022-04-26 08:47:32 +0100

    Set up local hardhat dev environment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -10,12 +8,26 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
-};
+ require("@nomiclabs/hardhat-waffle");
+
+ module.exports = {
+   solidity: "0.8.4",
+   networks: {
+     hardhat: {
+       chainId: 1337
+     },
+     // Mumbai testnet 
+     // mumbai: {
+     //   url: "https://rpc-mumbai.matic.today",
+     //   accounts: [process.env.pk]
+     // },
+     // Polygon mainnet
+     // polygon: {
+     //   url: "https://polygon-rpc.com/",
+     //   accounts: [process.env.pk]
+     // }
+   }
+ };
